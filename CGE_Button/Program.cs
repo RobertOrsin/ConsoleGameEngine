4a65a3ef2bf48228a8142113db16c406e5049572
Robert Zessin <robert.zessin@gmx.de>
2024-02-05 11:12:20 +0100

    Button-Class
    Added a Button-Class and example which detects mouse-clicks on a sprite and calls a function

---
+++ CGE_Button/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleGameEngine;
+using static ConsoleGameEngine.NativeMethods;
+
+namespace CGE_Button
+{
+
+    class CGE_Button : GameConsole
+    {
+
+        IntPtr inHandle;
+        delegate void MyDelegate();
+
+        Button button;
+        int counter = 0;
+
+        public CGE_Button()
+          : base(200, 120, "Fonts", fontwidth: 4, fontheight: 4)
+        { }
+        public override bool OnUserCreate()
+        {
+            inHandle = NativeMethods.GetStdHandle(NativeMethods.STD_INPUT_HANDLE);
+            uint mode = 0;
+            NativeMethods.GetConsoleMode(inHandle, ref mode);
+            mode &= ~NativeMethods.ENABLE_QUICK_EDIT_MODE; //disable
+            mode |= NativeMethods.ENABLE_WINDOW_INPUT; //enable (if you want)
+            mode |= NativeMethods.ENABLE_MOUSE_INPUT; //enable
+            NativeMethods.SetConsoleMode(inHandle, mode);
+
+            ConsoleListener.MouseEvent += ConsoleListener_MouseEvent;
+
+            ConsoleListener.Start();
+
+            TextWriter.LoadFont("fontsheet.txt", 7, 9);
+
+            button = new Button(40, 40, TextWriter.GenerateTextSprite("Click me!", TextWriter.Textalignment.Left, 1));
+            button.OnButtonClicked(ButtonClicked);
+
+            return true;
+        }
+        public override bool OnUserUpdate(TimeSpan elapsedTime)
+        {
+            Clear();
+
+            DrawSprite(button.x, button.y, button.sprite);
+
+            DrawSprite(0, 0, TextWriter.GenerateTextSprite($"Counter: {counter}", TextWriter.Textalignment.Left, 1, backgroundColor: (short)COLOR.TRANSPARENT, foregroundColor: (short)COLOR.FG_WHITE));
+
+            return true;
+        }
+
+        private void ConsoleListener_MouseEvent(MOUSE_EVENT_RECORD r)
+        {
+            button.Update(r);
+        }
+
+        public bool ButtonClicked()
+        {
+            counter++;
+
+            return true;
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(437);
+
+            using (var f = new CGE_Button())
+                f.Start();
+        }
+    }
+}
---
+++ ConsoleGameEngine/Button.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleGameEngine.NativeMethods;
+
+namespace ConsoleGameEngine
+{
+    public class Button
+    {
+        public int x, y, width, height;
+        public Sprite sprite;
+        Func<bool> method;
+
+        public Button(int x, int y, int width, int height, Sprite sprite)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.sprite = sprite;
+        }
+        public Button(int x, int y, Sprite sprite)
+        {
+            this.x = x;
+            this.y = y;
+            this.sprite = sprite;
+            width = sprite.Width;
+            height = sprite.Height;
+        }
+
+        public void OnButtonClicked(Func<bool> method)
+        {
+            this.method = method;
+        }
+
+        public void Update(MOUSE_EVENT_RECORD r)
+        {
+            int mouseX = r.dwMousePosition.X, mouseY = r.dwMousePosition.Y;
+            uint mouseState = r.dwButtonState;
+
+            if(mouseState == MOUSE_EVENT_RECORD.FROM_LEFT_1ST_BUTTON_PRESSED)
+            {
+                if(mouseX <= x + width && mouseX >= x && mouseY <= y + height && mouseY > y) 
+                {
+                    method();
+                }
+            }   
+        }
+    }
+}
